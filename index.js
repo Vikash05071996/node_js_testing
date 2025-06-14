6f9edbbfa53556c62e6e8ac2fab27ca5a0cee245
Vikash Singh <vikashrajpoot2995@gmail.com>
2025-06-14 14:16:35 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// app.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('✅ Hello from simple Node API!');
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
